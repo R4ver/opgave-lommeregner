0b222bafb64a458d52565a5d8439147b40f1a381
Frederik Jensen-Petersen <aspit1038@tietgen.dk>
2015-11-16 11:22:58 +0100

    Added Calc.addition

---
+++ calc1038.js
@@ -0,0 +1,15 @@
+class Calc {
+   constructor(x, y) {
+      this.x = x;
+      this.y = y;
+   }
+
+   static addition(val) {
+      return val.x + val.y;
+   }
+   
+}
+
+const calcThis = new Calc(5, 5);
+
+console.log(Calc.subtraction(calcThis));
